737cfc63b5e8f59ab4b1e15659bf1f0bf4e7bfa5
zekothedev <z.erassan@gmail.com>
2022-09-13 18:42:49 +0200

    Add welcomeMessage Program

---
+++ welcomeMessage.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Hi, What is your name?");
+string name = Console.ReadLine();
+Console.WriteLine("And how old are you ?");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Hello {name} you will be turning {age + 1} soon!");
